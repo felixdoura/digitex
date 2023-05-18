ad9ca37bf7110f7678b73274b2ad65727b10572a
Felix <felixdoura@hotmail.com>
2023-05-17 23:47:05 -0300

    Sigo avanzando con el singleproduct

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,8 +35,12 @@ const Login = () => {
                                     <div>
                                         <Link to='/forgot-password' >Olvide La Contraseña</Link>
                                         <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
-                                            <button className='button border-0' type='submit'>Login</button>
-                                            <Link to="/signup" className='button signup'>Registrarse</Link>
+                                            <button className='button border-0' type='submit'>
+                                                Login
+                                            </button>
+                                            <Link to="/signup" className='button signup'>
+                                                Registrarse
+                                            </Link>
                                         </div>
                                     </div>
                                 </form>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,24 +3,129 @@ import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta'
 import ProductCard from '../components/ProductCard'
 import ReactStars from 'react-rating-stars-component'
-
+import ReactImageZoom from 'react-image-zoom';
+import Color from '../components/Color';
 
 const SingleProduct = () => {
+    const props = {
+        width: 400,
+        height: 600,
+        zoomWidth: 600,
+        img: "https://m.media-amazon.com/images/I/515BNisImJL._SL1000_.jpg"
+    };
+
     const [orderedProduct, setorderedProduct] = useState(true);
     return (
         <>
             <Meta title={"Nombre del Producto"} />
             <BreadCrumb title="Nombre del Producto" />
-
             <div className='main-product-wrapper py-5 home-wrapper-2'>
                 <div className='container-xxl'>
                     <div className='row'>
-                        <div className='col-6'></div>
-                        <div className='col-6'></div>
+                        <div className='col-6'>
+                            <div className='main-product-image'>
+                                <div>
+                                    <ReactImageZoom {...props} />
+                                </div>
+                            </div>
+                            <div className='other-product-images d-flex flex-wrap gap-15'>
+                                <div>
+                                    <img src='https://m.media-amazon.com/images/I/515BNisImJL._SL1000_.jpg' alt='' className='img-fluid' />
+                                </div>
+                                <div>
+                                    <img src='https://m.media-amazon.com/images/I/515BNisImJL._SL1000_.jpg' alt='' className='img-fluid' />
+                                </div>
+                                <div>
+                                    <img src='https://m.media-amazon.com/images/I/515BNisImJL._SL1000_.jpg' alt='' className='img-fluid' />
+                                </div>
+                                <div>
+                                    <img src='https://m.media-amazon.com/images/I/515BNisImJL._SL1000_.jpg' alt='' className='img-fluid' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <div className='main-product-details'>
+                                <div className='border-bottom'>
+                                    <h3 className='title'>
+                                        Auriculares para niño Buck 10 de colores
+                                    </h3>
+                                </div>
+                                <div className='border-bottom py-3'>
+                                    <p className='price'>$ 100</p>
+                                    <div className='d-flex align-items-center gap-10'>
+                                        <ReactStars
+                                            count={5}
+                                            size={24}
+                                            value="3"
+                                            edit={false}
+                                            activeColor='#ffd700'
+                                        />
+                                        <p className='mb-0 t-review'>( 2 reseñas )</p>
+                                    </div>
+                                    <a className='review-btn' href='#review'>Escriba una reseña</a>
+                                </div>
+                                <div className='border-bottom py-3'>
+                                    <div className='d-flex gap-10 align-items-center my-2'>
+                                        <h3 className='product-heading'>Tipo :</h3>
+                                        <p className='product-data'>Reloj</p>
+                                    </div>
+                                    <div className='d-flex gap-10 align-items-center my-2'>
+                                        <h3 className='product-heading'>Marca :</h3>
+                                        <p className='product-data'>Havels</p>
+                                    </div>
+                                    <div className='d-flex gap-10 align-items-center my-2'>
+                                        <h3 className='product-heading'>Categoria :</h3>
+                                        <p className='product-data'>Reloj</p>
+                                    </div>
+                                    <div className='d-flex gap-10 align-items-center my-2'>
+                                        <h3 className='product-heading'>Etiquetas :</h3>
+                                        <p className='product-data'>Reloj</p>
+                                    </div>
+                                    <div className='d-flex gap-10 align-items-center my-2'>
+                                        <h3 className='product-heading'>Disponibilidad :</h3>
+                                        <p className='product-data'>En Stock</p>
+                                    </div>
+                                    <div className='d-flex gap-10 flex-column mt-2 mb-3'>
+                                        <h3 className='product-heading'>Talle :</h3>
+                                        <div className='d-flex flex-wrap gap-15'>
+                                            <span className='badge border border-1 bg-white text-dark border-secondary'>S</span>
+                                            <span className='badge border border-1 bg-white text-dark border-secondary'>M</span>
+                                            <span className='badge border border-1 bg-white text-dark border-secondary'>L</span>
+                                            <span className='badge border border-1 bg-white text-dark border-secondary'>XL</span>
+                                        </div>
+                                    </div>
+                                    <div className='d-flex gap-10 flex-column mt-2 mb-3'>
+                                        <h3 className='product-heading'>Color :</h3>
+                                        <Color />
+                                    </div>
+                                    <div className='d-flex align-items-center gap-15 flex-row mt-2 mb-3'>
+                                        <h3 className='product-heading'>Cantidad :</h3>
+                                        <div className=''>
+                                            <input
+                                                type='number'
+                                                name=''
+                                                min={1}
+                                                max={10}
+                                                className='form-control'
+                                                style={{ width: "70px" }}
+                                                id=''
+                                            />
+                                        </div>
+                                        <div className='d-flex align-items-center gap-30 ms-5'>
+                                            <button className='button border-0' type='submit'>
+                                                Agregar al Carrito
+                                            </button>
+                                            <button className='button signup'>
+                                                Comprar
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <div className='description-wrapper py-5 home-wrapper-2'>
                 <div className='container-xxl'>
                     <div className='row'>
@@ -36,11 +141,11 @@ const SingleProduct = () => {
                 </div>
             </div>
 
-            <section className='reviews-wrapper py-5 home-wrapper-2'>
+            <section className='reviews-wrapper home-wrapper-2'>
                 <div className='container-xxl'>
                     <div className='row'>
                         <div className='col-12'>
-                            <h3>Reseñas</h3>
+                            <h3 id='review'>Reseñas</h3>
                             <div className='review-inner-wrapper'>
                                 <div className='review-head d-flex justify-content-between align-items-end'>
                                     <div>
